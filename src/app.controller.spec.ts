275deec31d7631e4a37e3bf7c335b9ca63bd88b0
Pavel Glebik <g1ebikpasha@gmail.com>
2026-06-20 02:36:04 +0300

    GitHub CLI message

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -16,8 +16,8 @@ describe('AppController', () => {
 
   describe('root', () => {
     it('should return "Hi from a new VDS!!!"', () => {
-      if (process.version.startsWith('v24')) {
-        throw new Error('Node.js should be < 24');
+      if (process.version.startsWith('v25')) {
+        throw new Error('Node.js should be < 25');
       }
       expect(appController.getHello()).toBe('Hi from a new VDS!!!');
     });
