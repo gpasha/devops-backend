717ca2087a3635791973fcf800a0562f452b0ab8
Pavel Glebik <g1ebikpasha@gmail.com>
2026-07-27 21:41:02 +0300

    text updates
    Signed-off-by: Pavel Glebik <g1ebikpasha@gmail.com>

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hi from week7! Deployed for k8s! + Helm (Charts) + SOPS + AGE + ARGOCD!';
+    return 'Hi from week7! Deployed for k8s! + Helm (Charts) + SOPS + AGE + ARGOCD + deploy-argocd.yaml!';
   }
 }
